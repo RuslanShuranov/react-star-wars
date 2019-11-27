535463f96f7dd0abe0ca3ac2d10d6924bab1eb4a
Ruslan Shuranov <ruslanshuranov@gmail.com>
2019-11-27 15:34:47 +0200

    Updates

---
+++ precache-manifest.931a351b7a3dbeb83893ccbfe1cdda1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696c12f5ca7e18302282b0bf0133a64b",
+    "url": "/StarWars-DB/index.html"
+  },
+  {
+    "revision": "d0d760960d5c49eecdef",
+    "url": "/StarWars-DB/static/css/main.351a6092.chunk.css"
+  },
+  {
+    "revision": "db5084f96e90eef926ce",
+    "url": "/StarWars-DB/static/js/2.fb6248b5.chunk.js"
+  },
+  {
+    "revision": "d0d760960d5c49eecdef",
+    "url": "/StarWars-DB/static/js/main.a1db4afa.chunk.js"
+  },
+  {
+    "revision": "5398691f8dbc54020c34",
+    "url": "/StarWars-DB/static/js/runtime-main.4ba438f7.js"
+  }
+]);
